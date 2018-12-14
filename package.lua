eb3a426eb55eda7cfd0334624b4cb097eec560a4
Masayuki Sugahara <equaaqua@hotmail.com>
2018-12-14 13:19:19 +0900

    updated package info

--- package.lua
+++ package.lua
@@ -1,9 +1,18 @@
 return {
     name = 'plageoj/shiritolua',
     version = '1.0.0',
-    description = 'shiritori discord bot',
+    description = 'Shiritori Discord bot',
+    author = {
+        name = 'Masayuki Sugahara',
+        email = 'equaaqua@hotmail.com'
+    },
+    license = 'MIT',
+    tags = { 'lua', 'lit', 'luvit', 'discord' },
     dependencies = {
         'luvit/luvit',
         'SinisterRectus/discordia'
+    },
+    files = {
+        '**.lua'
     }
 }

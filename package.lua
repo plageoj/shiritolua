4697bc54ee265980e434c22f7586f281f322579c
Masayuki Sugahara <equaaqua@hotmail.com>
2018-12-14 12:49:34 +0900

    added package info

---
+++ package.lua
@@ -0,0 +1,9 @@
+return {
+    name = 'plageoj/shiritolua',
+    version = '1.0.0',
+    description = 'shiritori discord bot',
+    dependencies = {
+        'luvit/luvit',
+        'SinisterRectus/discordia'
+    }
+}

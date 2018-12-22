c9c4aabac287a5280d8bf66e23aab13a2dd44a16
Masayuki Sugahara <equaaqua@hotmail.com>
2018-12-23 01:08:54 +0900

    splited files for testing

--- boot.lua
+++ boot.lua
@@ -1,13 +1,11 @@
 local discordia = require 'discordia'
 local client = discordia.Client()
 
-local lastword, wordlist = '', {}
-
-local comboLtr, comboLng = {times = 0, letter = ''}, {times = 0, length = 0}
-local shibariLtrEndTime, shibariLngEndTime = 0, 0
-
 local config = require './config.lua'
 local ut = require './utils.lua'
+local judge = require './judge.lua'
+
+local msg
 
 client:on(
 	'ready',
@@ -19,7 +17,6 @@ client:on(
 client:on(
 	'messageCreate',
 	function(message)
-		local minutes = 60
 		-- 数字はダメ、コメントも反応しない
 		local content = message.content:gsub('[0-9]', ''):gsub('//.*', '')
 
@@ -31,90 +28,30 @@ client:on(
 				return '<#' .. itm .. '>' == message.channel.mentionString
 			end
 		)
-		-- Bot の発言と空メッセージはさよなら
-		if outOfKaya or message.author.bot or #content == 0 or #message.mentionedUsers ~= 0 or #message.mentionedChannels ~= 0 then
+		-- Bot の発言と他チャンネルはさよなら
+		if outOfKaya or message.author.bot then
+			return
+		end
+
+		if #content == 0 or #message.mentionedUsers ~= 0 or #message.mentionedChannels ~= 0 then
 			return
 		end
 
 		-- 絵文字が入ってたら論外
-		if ut.encode(message.content):match('%%F0%%') then
+		if message.content:match '\xf0' then
 			return
 		end
 
-
 		message.channel:broadcastTyping()
 
-		local hiragana, words, processed, suffix, yomilen = ut.process(content)
-		if hiragana == false then
-			return message.channel:send('我輩の辞書に「' .. content .. '」はありません。')
-		end
+		local reply = judge.judge(content)
 
-		local prefix = processed:sub(1, #lastword)
-
-		-- やっぱり ん もなしにしよう
-		if suffix == 'ん' then
-			return message.channel:send 'んで終わっています。'
-		end
-
-		-- しりとりになっていなければダメ
-		if #lastword ~= 0 and lastword ~= prefix then
-			return message.channel:send(hiragana .. '。[' .. lastword .. '] から始めてくださいよ。')
-		end
-
-		-- 文字縛り
-		if shibariLtrEndTime > os.time() and prefix ~= suffix and comboLtr.times < config.shibaredMessages then
-			return message.channel:send(
-				'[' .. comboLtr.letter .. '] 縛り持続中！残' .. tostring(math.ceil((shibariLtrEndTime - os.time()) / minutes)) .. '分'
-			)
-		end
-		-- 音数縛り
-		if shibariLngEndTime > os.time() and comboLng.length ~= yomilen and comboLng.times < config.shibaredMessages then
-			return message.channel:send(
-				tostring(comboLng.length) ..
-					'音縛り持続中！' ..
-						hiragana ..
-							'は' .. tostring(yomilen) .. '音。残' .. tostring(math.ceil((shibariLngEndTime - os.time()) / minutes)) .. '分'
-			)
-		end
-
-		-- 既出語はダメ
-		if ut.includes(wordlist, hiragana) then
-			return message.channel:send '残念、もう出てます。'
-		end
-		table.insert(wordlist, hiragana)
-		if #wordlist > config.historyLength then
-			table.remove(wordlist, 1)
-		end
-
-		lastword = suffix
-
-		-- 文字コンボ判定
-		if comboLtr.letter == suffix then
-			comboLtr.times = comboLtr.times + 1
-			suffix = suffix .. ' (' .. tostring(comboLtr.times + 1) .. ')'
-
-			if comboLtr.times == config.shibariThreshold and shibariLngEndTime <= os.time() then
-				shibariLtrEndTime = os.time() + config.shibariLasts * minutes
-				message.channel:send('[' .. lastword .. '] 縛り発動！残' .. tostring(config.shibariLasts) .. '分')
+		if reply then
+			if msg then
+				msg:delete()
 			end
-		else
-			comboLtr.times, comboLtr.letter = 0, suffix
+			msg = message:reply(reply)
 		end
-
-		-- 音数コンボ判定
-		if comboLng.length == yomilen then
-			comboLng.times = comboLng.times + 1
-
-			if comboLng.times == config.shibariThreshold and shibariLtrEndTime <= os.time() then
-				shibariLngEndTime = os.time() + config.shibariLasts * minutes
-				message.channel:send(tostring(yomilen) .. '音縛り発動！残' .. tostring(config.shibariLasts) .. '分')
-			end
-		else
-			comboLng.times, comboLng.length = 0, yomilen
-		end
-
-		-- 無事受理されました
-		message.channel:send(hiragana .. ' = ' .. tostring(yomilen) .. '音 [' .. suffix .. ']')
 	end
 )
 
---
+++ judge.lua
@@ -0,0 +1,227 @@
+local json = require 'json'
+local http = require 'coro-http'
+
+local config = require './config.lua'
+local ut = require './utils.lua'
+
+local _M = {}
+
+local lastword, wordlist = 'り', {}
+
+local comboLtr, comboLng = {times = 0, letter = ''}, {times = 0, length = 0}
+local shibariLtrEndTime, shibariLngEndTime = 0, 0
+
+local function yomiOf(kanji)
+    local hiragana
+    while true do
+        local _, body =
+            http.request(
+            'POST',
+            'https://labs.goo.ne.jp/api/hiragana',
+            {
+                {'Content-Type', 'application/json'}
+            },
+            json.encode {
+                app_id = config.yomiApiId,
+                sentence = kanji,
+                output_type = 'hiragana'
+            }
+        )
+        hiragana = json.decode(body)
+
+        -- API からレスポンスがなければ、2秒待って再試行する
+        if type(hiragana.converted) ~= 'string' then
+            rt.sleep(2)
+        else
+            break
+        end
+    end
+    return hiragana.converted
+end
+
+function encode(string)
+    local ret = ''
+    string = tostring(string)
+    for i = 1, #string do
+        local char = string:byte(i)
+        if char > 0x7f then
+            ret = ret .. string.format('%%%X', char)
+        else
+            ret = ret .. string.char(char)
+        end
+    end
+    return ret
+end
+
+local function inDic(dic, string)
+    local function buildGetUrl(url, query)
+        if not query then
+            return url
+        end
+        url = url .. '?'
+        for key, val in pairs(query) do
+            url = url .. '&' .. key .. '=' .. encode(val)
+        end
+        return url:gsub('?&', '?')
+    end
+
+    local _, body =
+        http.request(
+        'GET',
+        buildGetUrl(
+            'http://public.dejizo.jp/NetDicV09.asmx/SearchDicItemLite',
+            {
+                Word = string,
+                Dic = dic,
+                Scope = 'HEADWORD',
+                Match = 'EXACT',
+                Merge = 'AND',
+                Prof = 'XHTML',
+                PageSize = 1,
+                PageIndex = 0
+            }
+        ),
+        {
+            {'content-type', 'text/xml'}
+        }
+    )
+    if body:match '<TotalHitCount>(%d*)</TotalHitCount>' ~= '0' then
+        print(dic)
+        return true
+    else
+        return false
+    end
+end
+
+function _M.process(kanji)
+    -- 辞書になかったら諦める
+    if not (inDic('EdictJE', kanji) or inDic('wpedia', kanji) or inDic('EJdict', kanji)) then
+        return false
+    end
+    local hiragana = yomiOf(kanji)
+    local hiraganar = {
+        '[!-~]',
+        '\xe3\x82[\x97-\x9f]', -- 平仮名特殊記号 ゛ など
+        '・',
+        ' ',
+        '\xe3\x83[\xbd-\xbf]' -- カタカナ特殊記号
+    }
+    -- 記号を除く
+    for _, str in ipairs(hiraganar) do
+        hiragana = hiragana:gsub(str, '')
+    end
+    local resh = ''
+    for i = 1, #hiragana do
+        local mt = hiragana:sub(i, i + 2):match('\xe3[\x81-\x83][\x80-\xbf]')
+        if mt then
+            resh = resh .. mt
+        end
+    end
+
+    local processed = resh:gsub('ー', '')
+    local count, yomiLen = -3, math.floor(#resh / 3)
+    local smallLtr = {'ゃ', 'ゅ', 'ょ', 'っ', 'ぁ', 'ぃ', 'ぅ', 'ぇ', 'ぉ'}
+
+    -- 最終音は何バイトか？
+    if
+        ut.includes(
+            smallLtr,
+            function(itm)
+                return processed:find(itm, -5) ~= nil
+            end
+        )
+     then
+        count = -6
+    end
+
+    -- 「っ」は1音としてカウントする
+    table.remove(smallLtr, 4)
+    -- 「っ」以外は0音としてカウントする
+    for _, ltr in ipairs(smallLtr) do
+        local _, occurrences = resh:gsub(ltr, '')
+        yomiLen = yomiLen - occurrences
+    end
+
+    print(kanji, hiragana, processed, yomiLen)
+
+    return resh, processed, processed:sub(count), yomiLen
+end
+
+function _M.judge(content)
+    local hiragana, processed, suffix, yomilen = _M.process(content)
+    if hiragana == false then
+        return '我輩の辞書に「' .. content .. '」はありません。[' .. lastword .. ']'
+    end
+
+    local prefix = processed:sub(1, #lastword)
+    local function timediff(time)
+        return math.ceil((time - os.time()) / 60)
+    end
+
+    local judgements = {
+        {
+            cond = suffix == 'ん',
+            ret = 'んで終わっています。'
+        },
+        {
+            cond = #lastword ~= 0 and lastword ~= prefix,
+            ret = hiragana .. '。しりとりじゃないじゃん。'
+        },
+        {
+            cond = shibariLtrEndTime > os.time() and lastword ~= prefix and comboLtr.times < config.shibaredMessages,
+            ret = '[' .. comboLtr.letter .. '] 縛り！残' .. timediff(shibariLtrEndTime) .. '分'
+        },
+        {
+            cond = shibariLngEndTime > os.time() and comboLng.length ~= yomilen and
+                comboLng.times < config.shibaredMessages,
+            ret = comboLng.length .. '音縛り！' .. hiragana .. '=' .. yomilen .. '音。残' .. timediff(shibariLngEndTime) .. '分'
+        },
+        {
+            cond = ut.includes(wordlist, hiragana),
+            ret = '残念、もう出てます。'
+        }
+    }
+
+    for _, cd in ipairs(judgements) do
+        if cd.cond then
+            return cd.ret .. '[' .. lastword .. ']'
+        end
+    end
+
+    table.insert(wordlist, hiragana)
+    if #wordlist > config.historyLength then
+        table.remove(wordlist, 1)
+    end
+
+    lastword = suffix
+    local ret = ''
+
+    -- 文字コンボ判定
+    if comboLtr.letter == suffix then
+        comboLtr.times = comboLtr.times + 1
+        suffix = suffix .. ' (' .. suffix .. comboLtr.times + 1 .. ')'
+        if comboLtr.times == config.shibariThreshold and shibariLngEndTime <= os.time() then
+            shibariLtrEndTime = os.time() + config.shibariLasts * 60
+            ret = ret .. '[' .. lastword .. '] 縛り発動！残' .. tostring(config.shibariLasts) .. '分\n'
+        end
+    else
+        comboLtr.times, comboLtr.letter = 0, suffix
+    end
+
+    -- 音数コンボ判定
+    if comboLng.length == yomilen then
+        comboLng.times = comboLng.times + 1
+        suffix = suffix .. ' <' .. yomilen .. '音>'
+        if comboLng.times == config.shibariThreshold and shibariLtrEndTime <= os.time() then
+            shibariLngEndTime = os.time() + config.shibariLasts * 60
+            ret = ret .. yomilen .. '音縛り発動！残' .. tostring(config.shibariLasts) .. '分\n'
+        end
+    else
+        comboLng.times, comboLng.length = 0, yomilen
+    end
+
+    -- 無事受理されました
+    return ret .. hiragana .. ' [' .. suffix .. ']'
+end
+
+return _M

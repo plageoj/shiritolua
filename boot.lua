e01db96781d42f8af5cbf250d9c72c94be474ffc
Masayuki Sugahara <equaaqua@hotmail.com>
2025-05-06 16:03:50 +0900

    fix: works for kanji and hiragana

--- boot.lua
+++ boot.lua
@@ -2,74 +2,74 @@ local discordia = require 'discordia'
 local client = discordia.Client()
 
 if os.getenv('SHIRITOLUA_CONFIG') then
-	local file = io.open('./config.lua', 'w')
-	io.output(file)
-	io.write(os.getenv('SHIRITOLUA_CONFIG'))
-	io.close(file)
+    local file = io.open('./config.lua', 'w')
+    io.output(file)
+    io.write(os.getenv('SHIRITOLUA_CONFIG'))
+    io.close(file)
 end
 
 local config = require './config.lua'
-local ut = require './utils.lua'
+local util = require './utils.lua'
 local judge = require './judge.lua'
 
 client:on(
-	'ready',
-	function()
-		local nerr, yomi =
-			pcall(
-			function()
-				local msg =
-					client:getChannel(config.reactChannels[1]):getMessages():toArray(
-					'createdAt',
-					function(msg)
-						return msg.author.id == '522728315824635906' and
-							msg.content:match('%[[^%] ]*')
-					end
-				)
-				return table.remove(msg).content:match('%[([^%] ]*)')
-			end
-		)
-		if nerr then
-			print(yomi)
-			judge.setWord(yomi)
-		end
-	end
+    'ready',
+    function()
+        local err, yomi =
+            pcall(
+                function()
+                    local msg =
+                        client:getChannel(config.reactChannels[1]):getMessages():toArray(
+                            'createdAt',
+                            function(msg)
+                                return msg.author.id == '522728315824635906' and
+                                    msg.content:match('%[[^%] ]*')
+                            end
+                        )
+                    return table.remove(msg).content:match('%[([^%] ]*)')
+                end
+            )
+        if err then
+            print(yomi)
+            judge.setWord(yomi)
+        end
+    end
 )
 
 client:on(
-	'messageCreate',
-	function(message)
-		-- 対象チャンネルでなければ、さよなら
-		local outOfKaya =
-			not ut.includes(
-			config.reactChannels,
-			function(itm)
-				return '<#' .. itm .. '>' == message.channel.mentionString
-			end
-		)
-		-- Bot の発言と他チャンネルはさよなら
-		if
-			outOfKaya or message.author.bot or #message.mentionedUsers ~= 0 or
-				#message.mentionedChannels ~= 0
-		 then
-			return
-		end
+    'messageCreate',
+    function(message)
+        -- 対象チャンネルでなければ、反応しない
+        local outOfKaya =
+            not util.includes(
+                config.reactChannels,
+                function(item)
+                    return '<#' .. item .. '>' == message.channel.mentionString
+                end
+            )
+        -- Bot の発言と他チャンネルはさよなら
+        if
+            outOfKaya or message.author.bot or #message.mentionedUsers ~= 0 or
+            #message.mentionedChannels ~= 0
+        then
+            return
+        end
 
-		local content = ut.preprocess(message.content)
+        local content = util.preprocess(message.content)
 
-		if not content then
-			return
-		end
+        if not content then
+            return
+        end
 
-		local reply, unchik = judge.judge(content)
+        local reply, knowledge = judge.judge(content)
 
-		if reply then
-			if unchik then
-				reply = reply .. '\n' .. unchik
-			end
-			message:reply(reply)
-		end
-	end
+        if reply then
+            if knowledge then
+                reply = reply .. '\n' .. knowledge
+            end
+            message:reply(reply)
+        end
+    end
 )
 
 client:run('Bot ' .. config.discordBotToken)
